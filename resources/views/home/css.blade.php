f81ac632a86830113324e3ed0d9ecdcdc322733b
Myron <josephonyekachi77@gmail.com>
2022-11-20 11:23:55 -0800

    updated

---
+++ resources/views/home/css.blade.php
@@ -0,0 +1,8 @@
+<!-- bootstrap core css -->
+<link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+<!-- font awesome style -->
+<link href="home/css/font-awesome.min.css" rel="stylesheet" />
+<!-- Custom styles for this template -->
+<link href="home/css/style.css" rel="stylesheet" />
+<!-- responsive style -->
+<link href="home/css/responsive.css" rel="stylesheet" />
--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -13,14 +13,7 @@
     <meta name="author" content="" />
     <link rel="shortcut icon" href="home/images/favicon.png" type="">
     <title>Myron Stores</title>
-    <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
-    <!-- font awesome style -->
-    <link href="home/css/font-awesome.min.css" rel="stylesheet" />
-    <!-- Custom styles for this template -->
-    <link href="home/css/style.css" rel="stylesheet" />
-    <!-- responsive style -->
-    <link href="home/css/responsive.css" rel="stylesheet" />
+    @include('home.css')
 </head>
 
 <body>
