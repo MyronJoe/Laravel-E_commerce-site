9b2d18bdc6dace3cdca626fa42906fa883b923fb
Myron <josephonyekachi77@gmail.com>
2022-11-20 12:53:20 -0800

    updated

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,79 @@
+<nav class="navbar navbar-expand navbar-light navbar-bg">
+    <a class="sidebar-toggle js-sidebar-toggle">
+        <i class="hamburger align-self-center"></i>
+    </a>
+
+    <div class="navbar-collapse collapse">
+        <ul class="navbar-nav navbar-align">
+            <li class="nav-item dropdown">
+                <a class="nav-icon dropdown-toggle mt-2" href="#" id="alertsDropdown" data-bs-toggle="dropdown">
+                    <div class="position-relative">
+                        <i class="align-middle" data-feather="bell"></i>
+                        <span class="indicator">4</span>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="alertsDropdown">
+                    <div class="dropdown-menu-header">
+                        4 New Notifications
+                    </div>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item">
+                            <div class="row g-0 align-items-center">
+                                <div class="col-2">
+                                    <i class="text-danger" data-feather="alert-circle"></i>
+                                </div>
+                                <div class="col-10">
+                                    <div class="text-dark">Update completed</div>
+                                    <div class="text-muted small mt-1">Restart server 12 to complete the update.</div>
+                                    <div class="text-muted small mt-1">30m ago</div>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-icon dropdown-toggle mt-2" href="#" id="messagesDropdown" data-bs-toggle="dropdown">
+                    <div class="position-relative">
+                        <i class="align-middle" data-feather="message-square"></i>
+                    </div>
+                </a>
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="messagesDropdown">
+                    <div class="dropdown-menu-header">
+                        <div class="position-relative">
+                            4 New Messages
+                        </div>
+                    </div>
+                    <div class="list-group">
+                        <a href="#" class="list-group-item">
+                            <div class="row g-0 align-items-center">
+                                <div class="col-2">
+                                    <img src="admin/img/avatars/avatar-5.jpg" class="avatar img-fluid rounded-circle" alt="Vanessa Tucker">
+                                </div>
+                                <div class="col-10 ps-2">
+                                    <div class="text-dark">Vanessa Tucker</div>
+                                    <div class="text-muted small mt-1">Nam pretium turpis et arcu. Duis arcu tortor.</div>
+                                    <div class="text-muted small mt-1">15m ago</div>
+                                </div>
+                            </div>
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+            </li>
+
+            <li class="nav-item dropdown pr-4">
+                <x-app-layout>
+
+                </x-app-layout>
+            </li>
+
+
+        </ul>
+    </div>
+</nav>
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,78 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+            <span class="align-middle">AdminKit</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link d-flex d-flex" href="index.html">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="pages-profile.html">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="pages-sign-in.html">
+                    <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="pages-sign-up.html">
+                    <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="pages-blank.html">
+                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                </a>
+            </li>
+
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="ui-buttons.html">
+                    <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="ui-forms.html">
+                    <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="ui-cards.html">
+                    <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="ui-typography.html">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link d-flex" href="icons-feather.html">
+                    <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+                </a>
+            </li>
+
+
+        </ul>
+
+
+    </div>
+</nav>
