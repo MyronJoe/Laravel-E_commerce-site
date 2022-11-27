208a25d200c0203f2df41d49cb4440a35917bc3c
Myron <josephonyekachi77@gmail.com>
2022-11-27 15:52:11 -0800

    stripe payment functionality

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>>Myron Stores Payment Section</title>
+    <title>Myron Stores Payment Section</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
@@ -78,7 +78,7 @@
     
                         <div class="row">
                             <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now ($100)</button>
+                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now (${{$totalPrice}})</button>
                             </div>
                         </div>
                             
