cb26c2017247b6993be60a224466c9c49fafcc92
Myron <josephonyekachi77@gmail.com>
2022-11-29 23:16:34 -0800

    updated

---
+++ resources/views/home/order.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <link rel="shortcut icon" href="home/images/favicon.png" type="">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Myron Stores</title>
+    @include('home.css')
+</head>
+
+<body>
+    <div class="hero_area">
+        <!-- header section strats -->
+        @include('home.header')
+        <!-- end header section -->
+        <!-- slider section -->
+        @include('home.slider')
+        <!-- end slider section -->
+    </div>
+    <!-- why section -->
+    @include('home.why')
+    <!-- end why section -->
+
+    <!-- arrival section -->
+    @include('home.new_arrival')
+    <!-- end arrival section -->
+
+    <!-- product section -->
+    @include('home.product')
+    <!-- end product section -->
+
+    <!-- subscribe section -->
+    @include('home.subscribe')
+    <!-- end subscribe section -->
+    <!-- client section -->
+    @include('home.client')
+    <!-- end client section -->
+    <!-- footer start -->
+    @include('home.footer')
+    <!-- footer end -->
+    <div class="cpy_">
+        <p class="mx-auto">© 2021 All Rights Reserved By Myron Stores
+
+        </p>
+    </div>
+    <!-- jQery -->
+    <script src="home/js/jquery-3.4.1.min.js"></script>
+    <!-- popper js -->
+    <script src="home/js/popper.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="home/js/bootstrap.js"></script>
+    <!-- custom js -->
+    <script src="home/js/custom.js"></script>
+</body>
+
+</html>
