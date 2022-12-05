ae5d531983fb89465b34661d367f907bfe69065e
Myron <josephonyekachi77@gmail.com>
2022-12-04 22:39:40 -0800

    sweetalert

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -8,3 +8,5 @@
 <link href="{{ asset('home/css/responsive.css') }}" rel="stylesheet" />
 
 <link rel="shortcut icon" href="{{ asset('home/images/favicon.png') }}" type="">
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
