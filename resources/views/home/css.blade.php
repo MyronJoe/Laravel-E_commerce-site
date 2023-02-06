5622c96660fe08ce80d85512ad09dfef3b7dc25c
Myron <josephonyekachi77@gmail.com>
2023-02-05 22:39:38 -0800

    updated

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -2,9 +2,9 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('home/css/bootstrap.css') }}" />
 <!-- font awesome style -->
 <link href="{{ asset('home/css/font-awesome.min.css') }}" rel="stylesheet" />
-<!-- Custom styles for this template -->
+
 <link href="{{ asset('home/css/style.css') }}" rel="stylesheet" />
-<!-- responsive style -->
+
 <link href="{{ asset('home/css/responsive.css') }}" rel="stylesheet" />
 
 <link rel="shortcut icon" href="{{ asset('home/images/favicon.png') }}" type="">
