e3b5530469c28938de7a4c51e3c572f479217acb
Myron <josephonyekachi77@gmail.com>
2023-02-05 22:40:06 -0800

    updated

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -1,6 +1,5 @@
-<!-- bootstrap core css -->
 <link rel="stylesheet" type="text/css" href="{{ asset('home/css/bootstrap.css') }}" />
-<!-- font awesome style -->
+
 <link href="{{ asset('home/css/font-awesome.min.css') }}" rel="stylesheet" />
 
 <link href="{{ asset('home/css/style.css') }}" rel="stylesheet" />
